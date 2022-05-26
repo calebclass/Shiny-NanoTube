45372fb8b71395cb3517acfcc36e152c296936de
Caleb Class <caleb.a.class@gmail.com>
2022-05-26 14:47:33 -0400

    Use 'box' format for Help pages.

--- ui.R
+++ ui.R
@@ -29,16 +29,54 @@ dashboardPage(skin = "blue",
                 tabItems(
                   
                   tabItem(tabName = "Welcome",
-                          fluidPage(
-                            h1("Welcome to the NanoTube!"),
-                            
-                            
+                          fluidRow(
                             includeCSS("www/styling2.css"),
-                            includeHTML("www/landing.html"),
-                            includeScript("www/styling2.js")
                             
-                            # img(src="www/cophs_horiz_4cp_1-4w.jpg", align = "left")
+                            box(
+                              
+                              width = 10,
+                              
+                              withTags({
+                                div(class="norm", checked = NA,
+                                    h1("Welcome to the NanoTube!"),
+                                    img(src="flowchart.jpg", class = "resize", align = "left"),
+                                    
+                                    p("NanoTube performs data processing, quality control, normalization and analysis on NanoString gene expression data."),
+                                    b("Click on the Setup tab to get started."),
+                                    
+                                    br(),
+                                    br(),
+                                    a(href='http://www.bioconductor.org/packages/release/bioc/html/NanoTube.html', b("Check out the NanoTube package on Bioconductor!")),
+                                    p("This R package provides additional normalization and analysis options for NanoString nCounter data."),
+                                    
+                                    
+                                    h2("Basic Features"),
+                                    h3("Data Processing"),
+                                    p("nCounter data are input as raw RCC files or CSV files (which possibly came from the RCC Collector tool). An additional sample information table is then loaded to allow comparisons between groups."),
+                                    h3("Normalization"),
+                                    p("This application performs manufacturer-recommended normalization steps, including positive and housekeeping normalization, as well as the removal of target genes found to have expression levels below 'background' (estimated from the negative control gene expression)."),
+                                    h3("Analysis"),
+                                    p("nCounter data are input as raw RCC files or CSV files (possibly from the RCC Collector tool), which can then be combined with a sample information table and saved as an ExpressionSet."),
+                                    h3("Visualization"),
+                                    p("nCounter data are input as raw RCC files or CSV files (possibly from the RCC Collector tool), which can then be combined with a sample information table and saved as an ExpressionSet."),
+                                    
+                                    h2("Citation"),
+                                    p("If you use the NanoTube in your work, please cite our conference paper:"),
+                                    b("Class CA, Bristow CA, Do K-A (2021). Easy NanoString Gene Expression Analysis with the Nanotube. The FASEB Journal 36(S1)."),
+                                    
+                                    h2("License"),
+                                    p("The NanoTube is provided with the GNU General Public License (GPL-3), and without warranty."),
+                                    a(href="https://github.com/calebclass/Shiny-NanoTube/blob/master/LICENSE", b("GPL-3 License for NanoTube")),
+                                    br(),
+                                    br(),
+                                    br(),
+                                    
+                                    img(src="cophs_horiz_4cp_1-4w.jpg", class = "resize2", align = "left")
+                                )
+                              })
+                            )
                           )
+                          
                   ),
                   
                   tabItem(tabName = "setup",
@@ -202,93 +240,106 @@ dashboardPage(skin = "blue",
                                               )))
                   ),
                   
-                             tabItem(tabName = "AnalysisRes",
-                                                 fluidRow(
-                                                   column(width = 5,
-                                                          box(
-                                                            title = "PCA", width = NULL, 
-                                                            plotlyOutput("pcaPlot")
-                                                          ),
-                                                          
-                                                          box(
-                                                            title = "Volcano Plot", width = NULL,
-                                                            
-                                                            uiOutput("volUI"),
-                                                            numericInput('volcanoVertLineInput','log2(FC) cutoff', value = 0, min = 0, max = 10),
-                                                            numericInput('volcanoHorLineInput','p-val cutoff', value = 0.05, min = 0, max = 1),
-                                                            
-                                                            plotlyOutput("canoPlot")
-                                                          )
-                                                   ),
-                                                   column(width = 7,
-                                                          box(title = "Summary", width = NULL,
-                                                              column(width = 4, 
-                                                                     numericInput('summaryQ', 'q-val cutoff', value = 0.05, min = 0, max = 1)),
-                                                              tableOutput("deCounts")),
-                                                          
-                                                          box(
-                                                            title = "Full Results", width = NULL,
-
-                                                            DTOutput("deTab"),
-                                                            downloadButton("DEdownload","Download Table")
-                                                          ))
-                                                 )
-                                                 
-                                                 ####
-                                                 #tabPanel("Nanostring data table",
-                                                  #        fluidPage(
-                                                  #          h4("datatable"),
-                                                  #          tableOutput('nanoTable'),
-                                                  #          downloadButton("NANOdownload","Download Table")
-                                                  #          
-                                                  #        ))
-                                                 #####https://shiny.rstudio.com/reference/shiny/1.2.0/showTab.html
-                                                 #conditionalPanel(
-                                                 #  condition = "input.gsDb != null",
-                                                 #)
-                                                 
-                                                 
-                                      
-                             ),
-                             
-                             ####
-                             tabItem(tabName = "GSA",
-                                     fluidRow(
-                                       column(width = 4,
-                                              box(title = "GSEA Visualization Options", width = NULL,
-                                                uiOutput("gsUI")
-                                              )),
+                  tabItem(tabName = "AnalysisRes",
+                          fluidRow(
+                            column(width = 5,
+                                   box(
+                                     title = "PCA", width = NULL, 
+                                     plotlyOutput("pcaPlot")
+                                   ),
+                                   
+                                   box(
+                                     title = "Volcano Plot", width = NULL,
+                                     
+                                     uiOutput("volUI"),
+                                     numericInput('volcanoVertLineInput','log2(FC) cutoff', value = 0, min = 0, max = 10),
+                                     numericInput('volcanoHorLineInput','p-val cutoff', value = 0.05, min = 0, max = 1),
+                                     
+                                     plotlyOutput("canoPlot")
+                                   )
+                            ),
+                            column(width = 7,
+                                   box(title = "Summary", width = NULL,
+                                       column(width = 4, 
+                                              numericInput('summaryQ', 'q-val cutoff', value = 0.05, min = 0, max = 1)),
+                                       tableOutput("deCounts")),
+                                   
+                                   box(
+                                     title = "Full Results", width = NULL,
+                                     
+                                     DTOutput("deTab"),
+                                     downloadButton("DEdownload","Download Table")
+                                   ))
+                          )
+                          
+                          ####
+                          #tabPanel("Nanostring data table",
+                          #        fluidPage(
+                          #          h4("datatable"),
+                          #          tableOutput('nanoTable'),
+                          #          downloadButton("NANOdownload","Download Table")
+                          #          
+                          #        ))
+                          #####https://shiny.rstudio.com/reference/shiny/1.2.0/showTab.html
+                          #conditionalPanel(
+                          #  condition = "input.gsDb != null",
+                          #)
+                          
+                          
+                          
+                  ),
+                  
+                  ####
+                  tabItem(tabName = "GSA",
+                          fluidRow(
+                            column(width = 4,
+                                   box(title = "GSEA Visualization Options", width = NULL,
+                                       uiOutput("gsUI")
+                                   )),
+                            
+                            column(width = 8,
+                                   box(width = NULL,
                                        
-                                       column(width = 8,
-                                              box(width = NULL,
-
-                                                DTOutput("gsTab"),
-                                              ),
-                                              
-                                              box(width = NULL,
-                                                plotlyOutput("gsHM"),
-                                                
-                                                includeHTML("www/gsa.html")
-                                              )
-                                              )
-                                     )
-                                     
-                                     
-                                                 
-                                      
-                             ),
-                             #####
-                             tabItem("Help",
-                                      fluidPage(
-                                        includeCSS("www/styling2.css"),
-                                        includeHTML("www/help.html"),
-                                        includeScript("www/styling2.js")
-                                      )
-                                      
-                                      )
-                             
+                                       DTOutput("gsTab"),
+                                   ),
+                                   
+                                   box(width = NULL,
+                                       plotlyOutput("gsHM"),
+                                   )
+                            )
+                          )
+                          
+                          
+                          
+                          
+                  ),
+                  #####
+                  tabItem("Help",
+                          fluidRow(
+                            includeCSS("www/styling2.css"),
+                            
+                            box(title = "Setup", width = 10, 
+                                collapsible = TRUE, collapsed = TRUE,
+                            includeHTML("www/help_setup.html"),
+                            ),
+                            box(title = "Quality Control", width = 10, 
+                                collapsible = TRUE, collapsed = TRUE,
+                                includeHTML("www/help_qc.html"),
+                            ),
+                            box(title = "Analysis", width = 10, 
+                                collapsible = TRUE, collapsed = TRUE,
+                                includeHTML("www/help_de.html"),
+                            ),
+                            box(title = "Gene Set Analysis", width = 10, 
+                                collapsible = TRUE, collapsed = TRUE,
+                                includeHTML("www/help_gsa.html"),
+                            )
+                          )
+                          
                   )
-)
+                  
+                )
+              )
 )
 
 
